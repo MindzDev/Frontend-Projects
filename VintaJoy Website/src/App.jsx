653c1479ffaceb55d270b15895b8a8642a302214
Zunair Arif <zunair.arif992@gmail.com>
2024-04-22 05:48:07 +0500

    First Commit

---
+++ VintaJoy Website/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.scss'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+
+function App() {
+
+
+  return (
+    <>
+     <Header/>
+     <Hero/>
+    </>
+  )
+}
+
+export default App
